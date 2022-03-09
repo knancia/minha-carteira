69522ed70bc18b1e17ab996fa4f9073a1ea199b3
knancia <oliveira.augusto@outlook.com>
2022-03-08 22:31:45 -0300

    react-router-dom

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {  } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import List from '../pages/List';
+
+const AppRoutes: React.FC = () => (
+    <Switch>
+
+    </Switch>
+);
+
+export default AppRoutes()
